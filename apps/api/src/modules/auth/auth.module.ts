0eef245a8085e75a2148aae5f7e676539ed96dd1
jongsujin <jjs9836@naver.com>
2025-11-30 21:17:28 +0900

    feat: 회원가입 응답값 수정 및 JWT Guard 모듈 생성

--- apps/api/src/modules/auth/auth.module.ts
+++ apps/api/src/modules/auth/auth.module.ts
@@ -3,6 +3,9 @@ import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtAuthGuard } from 'src/core/common/guards/jwt-auth.guard';
+import { JwtStrategy } from 'src/core/common/strategies/jwt.strategy';
 
 @Module({
   imports: [
@@ -15,9 +18,13 @@ import { AuthService } from './auth.service';
       secret: process.env.JWT_REFRESH_SECRET ?? 'default-secret',
       signOptions: { expiresIn: '7d' },
     }),
+    PassportModule.register({
+      secret: process.env.JWT_ACCESS_SECRET ?? 'default-secret',
+      signOptions: { expiresIn: '1h' },
+    }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
-  exports: [AuthService],
+  providers: [AuthService, JwtStrategy, JwtAuthGuard],
+  exports: [AuthService, JwtAuthGuard],
 })
 export class AuthModule {}
